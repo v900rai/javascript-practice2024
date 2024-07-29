134362b1b732bb4b56ad2114488af7d803e73c7a
vishal rai <vishalrai10797@gmail.com>
2024-07-29 14:07:46 +0530

    javascript

---
+++ function.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+  s = a + b;
+  console.log("before return ");
+  return s;
+}
+
+let val = sum(3, 4);
+console.log(val);
---
+++ map.js
@@ -0,0 +1,5 @@
+let nums = [2, 4, 6, 8];
+let newArr = nums.map((val) => {
+  return val * 2;
+});
+console.log(newArr);
